f7f8c3d9b91c36c507c36369d7ac4c13dc50ac45
VekrisTizara <94655538+VekrisTizara@users.noreply.github.com>
2022-11-26 01:15:39 +0100

    get JSON from url, then get random amount of countries and show it with FAST API

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
+import urllib.request, json
+import random
 
 app = FastAPI()
+
+user_num = random.randint(5, 20)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/addresses")
+async def get_address():
+    list_countries = []
+    with urllib.request.urlopen("https://random-data-api.com/api/v2/addresses?size="+f"{user_num}") as url:
+        data = json.load(url)
+    for i in range(user_num):
+        list_countries.append(data[i]["country"])
+    return list_countries
+
+
+#delete random
