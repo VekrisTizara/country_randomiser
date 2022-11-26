401eeda1b323619a965fd1d61ae59ec11823fb17
VekrisTizara <94655538+VekrisTizara@users.noreply.github.com>
2022-11-26 02:16:24 +0100

    add loop to iterate through list of countries and get information about them from rest api

--- main.py
+++ main.py
@@ -1,24 +1,38 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import urllib.request, json
 import random
+import requests
+
 
 app = FastAPI()
 
 user_num = random.randint(5, 20)
+
+class Country(BaseModel):
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/addresses")
+@app.get("/countries")
 async def get_address():
     list_countries = []
     with urllib.request.urlopen("https://random-data-api.com/api/v2/addresses?size="+f"{user_num}") as url:
         data = json.load(url)
-    for i in range(user_num):
-        list_countries.append(data[i]["country"])
-    return list_countries
+    for country_num in range(user_num):
+        list_countries.append(data[country_num]["country"])
 
+    print(list_countries[0])
 
-#delete random
+    for i in range(len(list_countries)):
+        country = list_countries[i].replace(" ", "")
+        url_country = "https://restcountries.com/v3.1/name/"+f"{country}"
+        if requests.get(url_country).status_code == 404:
+            print(404)
+        else:
+            with urllib.request.urlopen(url_country) as url:
+                data = json.load(url)
+        return data
+
